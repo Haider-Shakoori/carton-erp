e8368f267a58d6c2fd816254f7dcc49ccf172f6b
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-21 08:32:10 +0430

    Add variance investigation permissions

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -52,7 +52,7 @@ class PermissionsSeeder extends Seeder
             'stock' => ['view', 'create', 'update', 'delete'],
             'products' => ['view', 'create', 'update', 'delete', 'print', 'send-whatsapp'],
             'stock movements' => ['view'], // NEW
-            'stock reconciliations' => ['view', 'create', 'update', 'submit', 'approve', 'post', 'cancel'],
+            'stock reconciliations' => ['view', 'create', 'update', 'submit', 'approve', 'post', 'cancel', 'investigate', 'resolve'],
 
             // ============================================================
             // FINANCE
