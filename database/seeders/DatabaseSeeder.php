26a455d38aeaedff4f10affd785f38abe3d2de78
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-21 14:30:25 +0430

    Seed customer carton BOM templates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -142,6 +142,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ClientCartonRawMaterialSeeder::class);
         $this->call(BoardProfileSeeder::class);
         $this->call(CustomerCartonSizeSeeder::class);
+        $this->call(CustomerCartonBomSeeder::class);
         // $this->call(ProductSeeder::class);
     }
 }
