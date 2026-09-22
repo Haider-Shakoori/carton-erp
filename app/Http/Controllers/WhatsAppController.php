3d88f596b86fbebf466c96aa68541cc8475ec88c
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-22 12:03:34 +0430

    Clean WhatsApp controller imports

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\WhatsappSession;
-use Illuminate\Http\Client\Response;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
