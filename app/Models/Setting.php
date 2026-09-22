f260a290ce0de0db8cab33fce102b5b179fdbb31
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-22 06:53:29 +0430

    Add production approval control setting

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -19,11 +19,13 @@ class Setting extends Model
         'currency',
         'separate_business_units_enabled',
         'default_business_unit_id',
+        'production_approval_required',
     ];
 
     protected $casts = [
         'separate_business_units_enabled' => 'boolean',
         'default_business_unit_id' => 'integer',
+        'production_approval_required' => 'boolean',
     ];
 
     public function defaultBusinessUnit()
