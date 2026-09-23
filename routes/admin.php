74c06b8ce183b63a91b4561a77088982c8e0e29c
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-23 19:15:21 +0430

    Route simplified BOM preview

--- routes/admin.php
+++ routes/admin.php
@@ -682,6 +682,7 @@ Route::middleware(['auth', InitializeBusinessUnitContext::class])->prefix('admin
     Route::get('/bom', [BOMController::class, 'index'])->name('bom.index')->middleware('permission.feedback:view bom');
     Route::get('/bom/create', [BOMController::class, 'create'])->name('bom.create')->middleware('permission.feedback:create bom');
     Route::get('/bom/create/advanced', [BOMController::class, 'advancedCreate'])->name('bom.create-advanced')->middleware('permission.feedback:create bom');
+    Route::post('/bom/simple/preview', [BOMController::class, 'previewSimple'])->name('bom.preview-simple')->middleware('permission.feedback:create bom');
     Route::post('/bom/simple', [BOMController::class, 'storeSimple'])->name('bom.store-simple')->middleware('permission.feedback:create bom');
     Route::post('/bom', [BOMController::class, 'store'])->name('bom.store')->middleware('permission.feedback:create bom');
     Route::get('/bom/{bom}', [BOMController::class, 'show'])->name('bom.show')->middleware('permission.feedback:view bom');
