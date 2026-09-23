a8114aea8fa25d6af2c96229de5403664bead05b
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-23 11:47:30 +0430

    Apply shared index UI system

--- resources/views/admin/purchase-orders/index.blade.php
+++ resources/views/admin/purchase-orders/index.blade.php
@@ -19,10 +19,11 @@
         }
     </style>
 
+    <link rel="stylesheet" href="{{ asset('css/admin-index.css') }}">
 @endsection
 
 @section('content')
-    <div class="container-fluid px-3 px-md-4">
+    <div class="container-fluid px-3 px-md-4 erp-index-ui">
 
         {{-- ============================================================
         PAGE HEADER
